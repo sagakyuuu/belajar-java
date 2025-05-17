a81e256e8808c3e5f95e05160020f9c7855e571c
sagakyuuu <sagalawiliam0@gmail.com>
2025-05-17 10:50:11 +0700

    02.Variable_dan_TipeData

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        /*
+        * Variabel  =>  bagian dari memori yang dapat berisi nilai data
+        * Tipe Data =>  Dibagi menjadi 2 yaitu:
+        *               1. Data Primitif
+        *               2. Referensi objek
+        *
+        */
     }
 }
